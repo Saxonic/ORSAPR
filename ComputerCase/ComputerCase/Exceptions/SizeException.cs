6fd5ca79152a1bda7c1aa61ea6932dd168cc6145
Saxonic <Saxonic@users.noreply.github.com>
2021-12-12 18:57:31 +0700

    Исключения при несоответсвии размеров с зависимостями

---
+++ ComputerCase/ComputerCase/Exceptions/SizeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ComputerCase.Exceptions
+{
+    public class SizeException : Exception
+    {
+        public SizeException()
+        {
+        }
+        
+        public SizeException(string message) : base(message)
+        {
+        }
+        
+        public SizeException(string message,Exception innerException) : base(message,innerException)
+        {
+        }
+    }
+}
