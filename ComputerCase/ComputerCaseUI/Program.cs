502b34ff3af01a0aa3fb083a7be166ee34dfb249
Saxonic <Saxonic@users.noreply.github.com>
2021-12-12 13:46:52 +0700

    inital comit

--- ComputerCase/ComputerCaseUI/Program.cs
+++ ComputerCase/ComputerCaseUI/Program.cs
@@ -17,6 +17,7 @@ namespace ComputerCaseUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
