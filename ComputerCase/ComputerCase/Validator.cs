8e8e4327d144d010104c774385c60235ff11b987
Saxonic <Saxonic@users.noreply.github.com>
2021-12-12 15:51:43 +0700

    Реализован валидатор

---
+++ ComputerCase/ComputerCase/Validator.cs
@@ -0,0 +1,18 @@
+﻿namespace ComputerCase
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Проверка, входит ли указанное число в заданный диапазон 
+        /// </summary>
+        /// <param name="max"></param>
+        /// <param name="min"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool Validate(double max, double min, double value)
+        {
+            return !(max < value) && !(min > value);
+        }
+        
+    }
+}
