99193da169457d90bc71a61ad7675301f8a3f61e
Saxonic <Saxonic@users.noreply.github.com>
2021-12-25 16:06:16 +0700

    Реализован интерфейс, содержащий методы необходимые для построения корпуса

---
+++ ComputerCase/ComputerCase/IBuilderProgramAPI.cs
@@ -0,0 +1,37 @@
+﻿namespace ComputerCase
+{
+    /// <summary>
+    /// Интерфейс, содержащий методы необходимые для построения корпуса
+    /// </summary>
+    public interface IBuilderProgramAPI
+    {
+        /// <summary>
+        /// Создать эскиз круга
+        /// </summary>
+        /// <param name="diameter">Диаметр</param>
+        /// <param name="x">Центр круга по Х</param>
+        /// <param name="y">Центр круга по Y</param>
+        void CreateCircle(double diameter, double x, double y);
+
+        /// <summary>
+        /// Создать эскиз прямоугольника
+        /// </summary>
+        /// <param name="startDotX">начальная точка по X</param>
+        /// <param name="startDotY">начальная точка по Y</param>
+        /// <param name="endDotX">начальная точка по X</param>
+        /// <param name="endDotY">начальная точка по Y</param>
+        void CreateRectangle(double startDotX, double startDotY, double endDotX, double endDotY);
+
+        /// <summary>
+        /// Выдавить эскиз круга
+        /// </summary>
+        /// <param name="depth">Глубина выдавливания</param>
+        void ExtrudeCutCircle(double depth);
+
+        /// <summary>
+        /// Построить выдавливанием из эскиза прямоугольника
+        /// </summary>
+        /// <param name="depth"></param>
+        void ExtrudeRectangle(double depth);
+    }
+}
