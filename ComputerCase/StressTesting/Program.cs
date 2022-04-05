e9cc3f3a83e5efdf22337006036a6d564c4bfac5
Saxonic <Saxonic@users.noreply.github.com>
2022-04-05 14:30:33 +0700

    Правки в ПЗ и стресс тестах

--- ComputerCase/StressTesting/Program.cs
+++ ComputerCase/StressTesting/Program.cs
@@ -8,10 +8,26 @@ namespace StressTesting
 {
     internal static class Program
     {
+        private const double BitsInGigabyte = 1073741824;
         private static void Main(string[] args)
         {
-            //TestKompas3D();
-            TestInventor();
+            TestKompas3D();
+            //TestInventor();
+        }
+
+        private static CaseParameters GetCaseParameters()
+        {
+            return new CaseParameters
+            {
+                FrontFansCount = 2,
+                FrontFansDiameter = 120,
+                Height = 391,
+                Length = 400,
+                MotherboardType = MotherboardType.ATX,
+                UpperFansCount = 3,
+                UpperFansDiameter = 120,
+                Width = 160
+            };
         }
 
         private static void TestInventor()
@@ -29,17 +45,7 @@ namespace StressTesting
             var builder = new CaseBuilder(apiService);
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            var caseParameters = new CaseParameters
-            {
-                FrontFansCount = 1,
-                FrontFansDiameter = 40,
-                Height = 400,
-                Length = 400,
-                MotherboardType = MotherboardType.ATX,
-                UpperFansCount = 1,
-                UpperFansDiameter = 40,
-                Width = 400
-            };
+            var caseParameters = GetCaseParameters();
             var streamWriter = new StreamWriter($"log{apiService}.txt", true);
             Process currentProcess = Process.GetCurrentProcess();
             var count = 0;
@@ -47,8 +53,8 @@ namespace StressTesting
             {
                 builder.CrateCase(caseParameters);
                 var computerInfo = new ComputerInfo();
-                var usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory) *
-                                 0.000000000931322574615478515625;
+                var usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory)/
+                                 BitsInGigabyte;
                 streamWriter.WriteLine(
                     $"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}");
                 streamWriter.Flush();
